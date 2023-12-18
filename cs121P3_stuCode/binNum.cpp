b78e70ee280b084231093cea4842de6cb5e77f9e
George Davis <davisgl055@ecc.edu>
2023-12-18 03:38:56 -0500

    Made adding function work with directions(presumably) made greet function. Finish Menu Functions Debug. Make more docs btw. Previous commmit went wrong I guess

--- cs121P3_stuCode/binNum.cpp
+++ cs121P3_stuCode/binNum.cpp
@@ -384,24 +384,6 @@ void  BinNum::shiftBinNumBy( int shiftNum, BinNum& initBinNum )
 }
 
 
-//*****************************************************************************
-// Function Name: GreetUser
-// Purpose: Greet the user and explain to them what to do
-// Parameters: None
-//   Input: None
-//   Input & Output: None
-//   Output: None
-// Return Value: None
-// Non-local Vairables Used:
-// Functions Called: 
-//
-//*****************************************************************************
-void GreetUsers() 
-{
-    cout << "Hello user. This program will add nad multiply 4 bit binary numbers" << endl << endl;
-    cout << "Do you wish to add or multiply the numbers?";
-}
-
 ///////////////////////////////////////////////////////////////////////////////
 
 
